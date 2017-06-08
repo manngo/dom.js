e2213b1c29d8a471d7f3a36c397b399200da28f2
manngo <mark@manngo.net>
2017-06-08 16:24:01 +1000

    Added Ajax, renamed methods

--- dom.js
+++ dom.js
@@ -2,7 +2,6 @@
 	================================================
 	DOM Manipulation Functions
 	================================================ */
-
 		var DOM={
 			//	Returns an element or true array of elements
 				select: function(selector) {
@@ -35,7 +34,7 @@
 				================================================ */
 
 			//	Add element to or around another
-				insertBefore: function(newElement,element) {
+				prefix: function(newElement,element) {
 					element.parentNode.insertBefore(newElement,element);
 				},
 				prepend: function(newElement,element) {
@@ -44,12 +43,12 @@
 				append: function(newElement,element) {
 					element.appendChild(newElement);
 				},
-				insertAfter: function(newElement,element) {
+				affix: function(newElement,element) {
 					element.parentNode.insertBefore(newElement, element.nextSibling);
 				},
 
 			//	Add html to or around another element
-				insertHTMLBefore: function(html,element) {
+				prefixHTML: function(html,element) {
 					element.insertAdjacentHTML('beforebegin', html);
 				},
 				prependHTML: function(html,element) {
@@ -58,7 +57,7 @@
 				appendHTML: function(html,element) {
 					element.insertAdjacentHTML('beforeend', html);
 				},
-				insertHTMLAfter: function(html,element) {
+				affixHTML: function(html,element) {
 					element.insertAdjacentHTML('afterend', html);
 				},
 
@@ -69,6 +68,84 @@
 					if(value===undefined) value=true;
 					if(!element.hasAttribute(attribute)) element.setAttribute(attribute,value);
 					else element.removeAttribute(attribute);
-				}
+				},
+
+			//	Wrapping Content
+
+				wrap: function(element,wrapper) {
+					element.parentNode.insertBefore(wrapper, element);
+					wrapper.appendChild(element);
+					return wrapper;
+				},
+
+				wrapAll: function(elements, wrapper) {
+					elements[0].parentNode.insertBefore(wrapper, elements[0]);
+					for(var i=0;i<elements.length;i++) wrapper.appendChild(elements[i]);
+					return wrapper;
+				},
+				unwrap: function(element,totally) {
+					var parent=element.parentNode;
+					parent.parentNode.insertBefore(element,parent);
+					if(totally) parent.parentNode.removeChild(parent);
+					return element;
+				},
+
+			//	Empty Element
+
+				empty: function(element) {
+					element.innerHTML='';
+				},
+
+			//	Scroll Position
+
+				scrollPosition: function() {
+					return {
+						left: window.pageXOffset || document.documentElement.scrollLeft,
+						top: window.pageYOffset || document.documentElement.scrollTop
+					};
+				},
+
+			//	Bounding Rectangle
+
+				boundingRect: function(element,documentRelative) {
+					var rect=element.getBoundingClientRect();
+					var pos=DOM.scrollPosition();
+					var scrollLeft=documentRelative?pos.left:0;
+					var scrollTop=documentRelative?pos.top:0;
+					return {
+						'x':			rect.left-scrollLeft,
+						'y':			rect.top-scrollTop,
+						'width':		rect.right-rect.left,
+						'height':		rect.bottom-rect.top,
+						'left':			rect.left-scrollLeft,
+						'top':			rect.top-scrollTop,
+						'right':		rect.right-scrollLeft,
+						'bottom':		rect.bottom-scrollTop,
+						'scrollLeft':	pos.left,
+						'scrollTop':	pos.top,
+					};
+				},
+
+			//	Ajax
+
+				get: function(url,callback,query) {
+					var xhr=new XMLHttpRequest();
+					if(query) url+='?'+query;
+					xhr.open('get',url,true);
+					xhr.onload=function() {
+						if(callback) callback(this.responseText);
+					}
+					xhr.send(null);
+				},
+				loadHTML: function(element,url,query) {
+					this.get(url,function(data) {
+						element.innerHTML=data;
+					},query);
+				},
+				loadText: function(element,url,query) {
+					this.get(url,function(data) {
+						element.textContent=data;
+					},query);
+				},
 
 		};
